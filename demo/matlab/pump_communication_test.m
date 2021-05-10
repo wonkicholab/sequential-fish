f65038c01a8d22911e0950baf5b0468a23fa5a46
MinBG <59925807+MinBG@users.noreply.github.com>
2021-05-10 14:04:54 +0900

    Update pump_communication_test.m

--- demo/matlab/pump_communication_test.m
+++ demo/matlab/pump_communication_test.m
@@ -2,8 +2,6 @@
 m = modbus('serialrtu','COM7','Parity','even');
 
 %%
-% 1 register holds 2 bytes data each.
-
 % unsigned short int (2bytes) == uint16
 % unsigned long int (4bytes) == uint32
 % float (4bytes) == single
@@ -12,9 +10,13 @@ m = modbus('serialrtu','COM7','Parity','even');
 % input register - read only
 % holding register - read/write
 
+% ---Notice---
+% When inputting the register number in the codebook, 
+% you should implement (register number)+1 value
+
 % bit-wise / decimal / hexadecimal 주의
 
-i1 = read(m,'inputregs',1023,5); % manufactorer
+i1 = read(m,'inputregs',1024,5); % manufactorer
 ret1 = double.empty;
 for i = 1:length(i1)
     ret1(2*i-1) = rem(i1(i),256);
@@ -23,18 +25,13 @@ end
 ret1 = char(ret1);
 
 %%
-h1 = read(m,'holdingregs',4022,1,'uint16'); % flow unit
-h2 = read(m,'holdingregs',4023,1,'uint16'); % direction
-h3 = read(m,'holdingregs',4029,1,'uint16'); % baud rate
-h4 = read(m,'holdingregs',4025,1,'uint16'); % running
-h5 = read(m,'holdingregs',4024,1,'uint16'); % full speed
+h1 = read(m,'holdingregs',4023,1,'uint16'); % flow unit
+h2 = read(m,'holdingregs',4024,1,'uint16'); % direction
+h3 = read(m,'holdingregs',4030,1,'uint16'); % baud rate
+h4 = read(m,'holdingregs',4024,1,'uint16'); % full speed
 
-% read(m,'holdingregs', 4017,1,'uint16')
+write(m,'holdingregs',4026,1,'uint16'); % run
+write(m,'holdingregs',4026,0,'uint16'); % stop
 
-% write(m,'holdingregs',4025,1,'uint16'); % run
-% write(m,'holdingregs',4025,0,'uint16'); % stop
-
-% swapbytes(typecast(uint16(500),'single'));
-%%
+%set flow rate of time despense mode to 300
 write(m,'holdingregs',4169,double(typecast(single(str2double('300')),'uint16')),'uint16');
-
